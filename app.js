f1353452b2fa182ced15e2d763b45a2429c0d6c5
BoonRealCuaaaaa <99378331+BoonRealCuaaaaa@users.noreply.github.com>
2023-11-18 17:48:44 +0700

    Add all event

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-let table = new DataTable('#myTable', {
-    scrollResize: true,
-    scrollCollapse: true,
-    paging: false,
+let table = new DataTable("#myTable", {
+  scrollResize: true,
+  scrollCollapse: true,
+  paging: true,
 });
 
-table.on('click', 'tbody tr', (e) => {
-    let classList = e.currentTarget.classList;
- 
-    if (classList.contains('selected')) {
-        classList.remove('selected');
-    }
-    else {
-        table.rows('.selected').nodes().each((row) => row.classList.remove('selected'));
-        classList.add('selected');
-    }
-});
- 
-document.querySelectorAll('.toBCVP').forEach(item => {
-    item.addEventListener('click', () => {
-        var triggerEl = document.querySelector('#myTab #report-tab');
-        bootstrap.Tab.getOrCreateInstance(triggerEl).show();
+// table.on('click', 'tbody tr', (e) => {
+//     let classList = e.currentTarget.classList;
 
-        //Cap nhat du lieu
-    });
-})
+//     if (classList.contains('selected')) {
+//         classList.remove('selected');
+//     }
+//     else {
+//         table.rows('.selected').nodes().each((row) => row.classList.remove('selected'));
+//         classList.add('selected');
+//     }
+// });
+
+document.querySelectorAll(".toBCVP").forEach((item) => {
+  item.addEventListener("click", () => {
+    var triggerEl = document.querySelector("#myTab #report-tab");
+    bootstrap.Tab.getOrCreateInstance(triggerEl).show();
+
+    //Cap nhat du lieu
+  });
+});
 
 /*document.querySelector('#button').addEventListener('click', function () {
     table.row('.selected').remove().draw(false);
 });*/
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYm9vbnJlYWwiLCJhIjoiY2xvOWZ0eXQ2MDljNzJybXRvaW1oaXR3NyJ9.iu4mRTZ3mUFb7ggRtyPcWw';
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
 
 function onSubmit(token) {
-    document.getElementById("report-form").submit();
-  }
+  document.getElementById("report-form").submit();
+}
